8805adcae9609b493da2765f407b20167bd8e882
hossein <hossien.alipour@gmail.com>
2020-05-23 20:39:08 +0000

    add variables

---
+++ util/variables.js
@@ -0,0 +1,7 @@
+export const droppable = "droppable";
+export const draggable = "data-CoC-draggable";
+export const selectable = "data-CoC-selectable";
+export const editable = "data-CoC-editable";
+export const hoverable = "data-CoC-hoverable";
+export const name = "data-CoC-name";
+export const group_name = "data-group_name";
