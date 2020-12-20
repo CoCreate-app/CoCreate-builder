c0fb111865ee352086c1fd56902a49ff8cb28fdd
Frank Pagan <frank@cocreate.app>
2020-12-20 21:08:05 +0000

    clean file tree

--- dispath.js
+++
@@ -1,7 +0,0 @@
-if (window.parent !== window) {
-  console.log("dispath 1");
-  window.addEventListener("load", () => {
-    console.log("dispath load event");
-    window.parent.dispatchEvent(new Event("CoCreateHtmlTags-rendered"));
-  });
-}
