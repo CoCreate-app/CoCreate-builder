49d825578bc578757473dffc8bf683589fae31e7
frank <frank@cocreate.app>
2021-04-21 22:29:32 +0000

    clean script tags from docs and demo

--- archive/findPosition.js
+++ archive/findPosition.js
@@ -3,7 +3,7 @@
 // 	<head>
     // <link rel=stylesheet href="//cdn.quilljs.com/1.3.6/quill.snow.css" async defer>
     // <link rel="stylesheet" href="//cdn.quilljs.com/1.3.6/quill.bubble.css">
-    // <link rel="stylesheet" href="https://cdn.cocreate.app/latest/CoCreate.min.css" type="text/css" />
+    // <link rel="stylesheet" href="https://cdn.cocreate.app/latest/CoCreate.min.css" type="text/css"/>
       
 
 // 	</head>
