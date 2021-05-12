9b1b78506920dc52885c3e76faa8914c2b020d2b
frank <frank@cocreate.app>
2021-05-12 17:13:25 +0000

    updating config; add ccHost

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,37 +1,39 @@
 module.exports = {
-	"config": {
-		"apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
-		"securityKey": "f26baf68-e3a9-45fc-effe-502e47116265",
-		"organization_Id": "5de0387b12e200ea63204d6c",
-		"host": "server.cocreate.app:8088"
-	},
-
-    "sources": [{
+    "config": {
+        "apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
+        "securityKey": "f26baf68-e3a9-45fc-effe-502e47116265",
+        "organization_Id": "5de0387b12e200ea63204d6c",
+        "host": "server.cocreate.app:8088"
+    },
+    "sources": [
+        {
             "entry": "./docs/index.html",
             "collection": "files",
             "document_id": "60271be65e697f4f26a98a55",
             "key": "src",
-            "data":{
+            "data": {
                 "name": "builder Doc",
-                "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
                 "path": "/docs/builder"
             }
+        },
+        {
+            "entry": "./components/editor-panel.html",
+            "collection": "module-views",
+            "document_id": "5fb16c742fc8e61e01b43c15",
+            "key": "html",
+            "data": {
+                "name": "builder editor panel",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ]
+            }
         }
-    ],
-
-	"extract": {
-		"directory": "./src/",
-		"extensions": [
-			"js",
-			"css",
-			"html"
-		],
-		"ignores": [
-			"node_modules",
-			"vendor",
-			"bower_components",
-			"archive"
-		]
-	}
+    ]
 }
-
