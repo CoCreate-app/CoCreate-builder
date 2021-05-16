87d3905bdcf0436a3ff4fbd3b523c780b41fba5d
frank <frank@cocreate.app>
2021-05-16 14:31:24 +0000

    fix select init

--- src/components/initSelected.js
+++ src/components/initSelected.js
@@ -10,7 +10,7 @@ export default resolveCanvas.then(function({ crdtCon, canvas, canvasDocument, ca
     target: "[data-attributes]:not(.styleunit)",
     callback: (element, target) => {
       target.setAttribute('name', target.id + '-' + element.getAttribute('data-element_id'))
-      target.setAttribute('data-attributes_target', `#${canvas.id};[data-element_id=${element.getAttribute('data-element_id')}]`);
+      target.setAttribute('data-attributes_target', `#${canvas.id};[data-element_id="${element.getAttribute('data-element_id')}"]`);
     }
   });
 
