8cd0ef692683b9a6064433c1334ae6e01db3c945
frank <frank@cocreate.app>
2021-05-16 17:06:33 +0000

    clean init

--- src/components/resolveCanvas.js
+++ src/components/resolveCanvas.js
@@ -38,12 +38,11 @@
   canvasDocument.ccdefaultView = canvasWindow;
 
 
-
   export default new Promise(function(resolve, reject) {
 
 
     setTimeout(() => {
-      // crdt.replaceText({ crud: false, ...crdtCon, value: defaultHtml })
+      crdt.replaceText({ crud: false, ...crdtCon, value: defaultHtml })
       let html = crdt.getText({ crud: false, ...crdtCon });
 
 
@@ -55,7 +54,7 @@
           apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
           securityKey: 'f26baf68-e3a9-45fc-effe-502e47116265',
           organization_Id: '5de0387b12e200ea63204d6c'
-        }`
+        }`;
       canvasDocument.head.appendChild(apiInfo);
 
       let canvasScript = document.createElement('script');
--- src/index.js
+++ src/index.js
@@ -22,49 +22,9 @@ import './components/initToolbar'
 import './components/initVdom'
 import './components/initSelected'
 
-// import crdt from '@cocreate/crdt'
+import crdt from '@cocreate/crdt'
 
 
 
 
-
-let defaultHtml = `<!DOCTYPE html><html>
-	<head>
-  <style>body {background: red;}</style>
-	
-	</head>
-	<body data-element_id="body" style="padding:1;">
-		
-		<h1 data-element_id="t1" name="1">test 1</h1>
-		<h1 data-element_id="t3" name="3">test 3</h1>
-		<h1 data-element_id="t2" name="2">test 2</h1>
-		<h1 data-element_id="t4" name="4">test 4</h1>
-			
-
-        
-   
-	</body>
-</html>`;
-
-
-(async function init() {
-  // if (hasInit)
-  //   return
-  if (document.readyState === 'loading')
-    window.addEventListener("load", async(e) => {
-      await initBuilder()
-    })
-  else
-    await initBuilder()
-
-
-})()
-
-
-async function initBuilder() {
-
-
-}
-
-
-export default { }
+export default { crdt }
