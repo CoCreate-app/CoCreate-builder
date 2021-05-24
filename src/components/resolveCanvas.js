4831001c46a078617e352508cb37466e3bdeef68
frank <frank@cocreate.app>
2021-05-24 00:48:13 +0000

    resolve canvas by srcdoc

--- src/components/resolveCanvas.js
+++ src/components/resolveCanvas.js
@@ -1,13 +1,13 @@
   /*global DOMParser, Event*/
   import crdt from '@cocreate/crdt';
 
-function sleep(tt){
-  return new Promise(function(resolve){
-    setTimeout(()=>{
-      resolve()
-    }, tt)
-  })
-}
+  function sleep(tt) {
+    return new Promise(function(resolve) {
+      setTimeout(() => {
+        resolve()
+      }, tt)
+    })
+  }
 
   let defaultHtml = `<!DOCTYPE html><html>
   
@@ -17,8 +17,16 @@ function sleep(tt){
 		<h1 data-element_id="t3" name="3">test 3</h1>
 		<h1 data-element_id="t2" name="2">test 2</h1>
 		<h1 data-element_id="t4" name="4">test 4</h1>
-			
-
+		
+		<script>
+        var config = {
+            apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
+            organization_Id: '5de0387b12e200ea63204d6c',
+            host: 'wss://server.cocreate.app:8088'
+        }
+    </script>
+    
+     <script src="./CoCreate-builder-canvas.js"></script>
         
    
 	</body>
@@ -40,48 +48,64 @@ function sleep(tt){
   crdt.init(crdtCon);
 
 
-  canvasWindow = canvas.contentWindow;
-  canvasDocument = canvasWindow.document || canvas.contentDocument;
-  canvasDocument.ccdefaultView = canvasWindow;
-
-
   export default new Promise(async function(resolve, reject) {
 
 
     setTimeout(() => {
-      
-      while(true)
-      {
-        let html = crdt.getText({ crud: false, ...crdtCon });
-        if(html)
-          crdt.replaceText({ crud: false, ...crdtCon, value: '' });
-          else
-          break;
-          sleep(200);
-      }
-      crdt.replaceText({ crud: false, ...crdtCon, value: defaultHtml })
-      
+        if (window.location.href.endsWith('/CoCreate-builder/dist/index.html'))
+        {
+          
+          while (true) {
+            let html = crdt.getText({ crud: false, ...crdtCon });
+            if (html)
+              crdt.replaceText({ crud: false, ...crdtCon, value: '' });
+            else
+              break;
+            sleep(200);
+          }
+          crdt.replaceText({ crud: false, ...crdtCon, value: defaultHtml })
+        }
+
       let html = crdt.getText({ crud: false, ...crdtCon });
 
-      canvasDocument.documentElement.remove();
-      canvasDocument.append((new DOMParser().parseFromString(html, "text/html")).documentElement);
-    
-      let apiInfo = document.createElement('script');
-      apiInfo.innerHTML = `     var config = {
-          apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
-          securityKey: 'f26baf68-e3a9-45fc-effe-502e47116265',
-          organization_Id: '5de0387b12e200ea63204d6c'
-        }`;
-      canvasDocument.head.appendChild(apiInfo);
+      // canvasDocument.documentElement.remove();
+      // canvasDocument.append((new DOMParser().parseFromString(html, "text/html")).documentElement);
 
-      let canvasScript = document.createElement('script');
-      canvasScript.setAttribute('src', './CoCreate-builder-canvas.js');
-      canvasScript.addEventListener('load', function() {
-        canvasWindow.dispatchEvent(new Event("DOMContentLoaded", { "bubbles": true, "cancelable": false }))
-        canvasWindow.dispatchEvent(new Event("load", { "bubbles": true, "cancelable": false }))
+      // let apiInfo = document.createElement('script');
+      // apiInfo.innerHTML = `     var config = {
+      //     apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
+      //     securityKey: 'f26baf68-e3a9-45fc-effe-502e47116265',
+      //     organization_Id: '5de0387b12e200ea63204d6c'
+      //   }`;
+      // canvasDocument.head.appendChild(apiInfo);
+
+      // let canvasScript = document.createElement('script');
+      // canvasScript.setAttribute('src', './CoCreate-builder-canvas.js');
+      // canvasScript.addEventListener('load', function() {
+      //   canvasWindow.dispatchEvent(new Event("DOMContentLoaded", { "bubbles": true, "cancelable": false }))
+      //   canvasWindow.dispatchEvent(new Event("load", { "bubbles": true, "cancelable": false }))
+      // })
+      // canvasDocument.head.appendChild(canvasScript);
+
+      let newIframe = document.createElement('iframe');
+      newIframe.srcdoc = html;
+
+      // canvas.appendChild(canvasDocument);
+
+
+      for (let att of canvas.attributes) {
+        newIframe.setAttribute(att.name, att.value);
+      }
+
+      canvas.replaceWith(newIframe);
+      newIframe.addEventListener('load', () => {
+        newIframe.removeAttribute('srcdoc')
+        canvasWindow = newIframe.contentWindow;
+        canvasDocument = canvasWindow.document || newIframe.contentDocument;
+        canvasDocument.ccdefaultView = canvasWindow;
+        resolve({ crdtCon, canvas: newIframe, canvasDocument, canvasWindow })
       })
-      canvasDocument.head.appendChild(canvasScript);
-      resolve({ crdtCon, canvas, canvasDocument, canvasWindow })
+
 
     }, 2000)
   })
