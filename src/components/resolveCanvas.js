b68b2244624e3af4c4d2ed7d453266683bf4cf2c
frank pagan <frank@cocreate.app>
2021-08-18 05:47:22 +0000

    fix: canvas uses existing iframe rather than replacing

--- src/components/resolveCanvas.js
+++ src/components/resolveCanvas.js
@@ -1,99 +1,85 @@
-  import crdt from '@cocreate/crdt';
-  import uuid from '@cocreate/uuid';
+import crdt from '@cocreate/crdt';
+import uuid from '@cocreate/uuid';
 
-  function sleep(tt) {
-    return new Promise(function(resolve) {
-      setTimeout(() => {
-        resolve()
-      }, tt)
-    })
-  }
-    
-  let canvas, canvasDocument, canvasWindow, crdtCon;
+function sleep(tt) {
+	return new Promise(function(resolve) {
+		setTimeout(() => {
+			resolve()
+		}, tt)
+	})
+}
 
-  canvas = document.querySelector("#canvas");
-  if (!canvas)
-    console.error("builder config failed, can not find canvas iframe");
+let canvas, canvasDocument, canvasWindow, crdtCon;
 
-  crdtCon = {
-    collection: canvas.getAttribute('collection'),
-    document_id: canvas.getAttribute('document_id'),
-    name: canvas.getAttribute('name'),
-  };
+canvas = document.querySelector("#canvas");
+if(!canvas)
+	console.error("builder config failed, can not find canvas iframe");
 
-  export default new Promise(async function(resolve, reject) {
+crdtCon = {
+	collection: canvas.getAttribute('collection'),
+	document_id: canvas.getAttribute('document_id'),
+	name: canvas.getAttribute('name'),
+};
 
-    window.addEventListener('cocreate-crdt-update', initCanvas, true);
- 
-    window.crdtCon = crdtCon;
-    
-    crdt.init(crdtCon);
-  
-    function initCanvas() { 
-      try {
-        let info = event.detail;
-        let collection = info['collection'];
-        let document_id = info['document_id'];
-        let name = info['name'];
-      	if (collection == crdtCon.collection && document_id == crdtCon.document_id && name == crdtCon.name) {
-          let html = info.eventDelta.find(x => x.insert); 
-          let src = html.insert;
-          if(src) {
-          
-            let eid = elementId(src);
-            if (eid == false) return;
-              createIframe(src);
-          }
-      	}
-      }
-      catch (err) {
-        console.log('canvas init: ' + err);
-      }
-    }
-    
-    function elementId(src) { 
-      try { 
-        var parser = new DOMParser();
-        var dom = parser.parseFromString(src, "text/html");
-    
-        let elements = dom.querySelectorAll('*:not(html, [element_id])');
-        
-        for(let el of elements) {
-          if (el.getAttribute('element_id') == null) {
-            el.setAttribute('element_id', uuid.generate(6));
-          }  
-        }
-        
-        let html = dom.documentElement.outerHTML;
-        
-        if (elements.length > 0) {
-          crdt.replaceText({ crud: false, ...crdtCon, value: html });
-          elementId = function(){};
-          return false;
-        } 
-      }
-      catch (err) {
-        console.log('canvas init: ' + err);
-      }
-    }
-    
-    function createIframe(src) { 
-      let newIframe = document.createElement('iframe');
-      newIframe.srcdoc = src;
+export default new Promise(async function(resolve, reject) {
 
-      for (let att of canvas.attributes) {
-        newIframe.setAttribute(att.name, att.value);
-      }
+	window.addEventListener('cocreate-crdt-update', initCanvas, true);
 
-      canvas.replaceWith(newIframe);
-      newIframe.addEventListener('load', () => {
-        newIframe.removeAttribute('srcdoc')
-        canvasWindow = newIframe.contentWindow;
-        canvasDocument = canvasWindow.document || newIframe.contentDocument;
-        canvasDocument.ccdefaultView = canvasWindow;
-        resolve({ crdtCon, canvas: newIframe, canvasDocument, canvasWindow });
-      });
-      window.removeEventListener('cocreate-crdt-update', initCanvas, true);
-    }
-  })
-  
+	window.crdtCon = crdtCon;
+
+	crdt.init(crdtCon);
+
+	function initCanvas() {
+		try {
+			let info = event.detail;
+			let collection = info['collection'];
+			let document_id = info['document_id'];
+			let name = info['name'];
+			if(collection == crdtCon.collection && document_id == crdtCon.document_id && name == crdtCon.name) {
+				let html = info.eventDelta.find(x => x.insert);
+				let src = html.insert;
+				if(src) {
+
+					let eid = elementId(src);
+					if(eid == false) return;
+					canvasWindow = canvas.contentWindow;
+					canvasDocument = canvasWindow.document;
+					canvasDocument.ccdefaultView = canvasWindow;
+					canvasDocument.documentElement.innerHTML = src;
+					window.removeEventListener('cocreate-crdt-update', initCanvas, true);
+					resolve({ crdtCon, canvas, canvasDocument, canvasWindow });
+				}
+			}
+		}
+		catch(err) {
+			console.log('canvas init: ' + err);
+		}
+	}
+
+	function elementId(src) {
+		try {
+			var parser = new DOMParser();
+			var dom = parser.parseFromString(src, "text/html");
+
+			let elements = dom.querySelectorAll('*:not(html, [element_id])');
+
+			for(let el of elements) {
+				if(el.getAttribute('element_id') == null) {
+					el.setAttribute('element_id', uuid.generate(6));
+				}
+			}
+
+			let html = dom.documentElement.outerHTML;
+
+			if(elements.length > 0) {
+				crdt.replaceText({ crud: false, ...crdtCon, value: html });
+				elementId = function() {};
+				return false;
+			}
+		}
+		catch(err) {
+			console.log('canvas init: ' + err);
+		}
+	}
+
+})
