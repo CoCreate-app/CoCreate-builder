a4ad0e0200a13aa6cced1bd654ca5676fa122665
frank pagan <frank@cocreate.app>
2021-09-09 02:15:33 +0000

    fix: CoCreate.config directory, path, name and  public

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,126 +1,42 @@
 module.exports = {
     "config": {
-        "apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
-        "organization_Id": "5de0387b12e200ea63204d6c",
-        "host": "server.cocreate.app:8088"
+        "apiKey": "2061acef-0451-4545-f754-60cf8160",
+        "organization_Id": "5ff747727005da1c272740ab",
+        "host": "general.cocreate.app"
     },
-    "sources": [
-        {
-            "entry": "./demo/index.html",
+    
+    "sources": [{
+            "entry": "./docs/index.html",
             "collection": "files",
             "document_id": "60c144278fb9652e2dfd839f",
             "key": "src",
-            "data": {
-                "name": "Builder",
+            "data":{
+                "name": "index.html",
+                "path": "/docs/builder/index.html",
                 "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
+                    "general.cocreate.app"
                 ],
-                "path": "/builder"
-            }
-        },
-        {
-            "entry": "./docs/index.html",
-            "collection": "files",
-            "document_id": "60271be65e697f4f26a98a55",
-            "key": "src",
-            "data": {
-                "name": "builder Doc",
-                "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
-                ],
-                "path": "/docs/builder"
-            }
-        },
-        {
-            "entry": "./demo/components/nav.html",
-            "collection": "components",
-            "document_id": "60c26fdd7492af700086929c",
-            "key": "html",
-            "data": {
-                "name": "builder nav",
-                "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
-                ]
-            }
-        },
-        {
-            "entry": "./demo/components/left-panel.html",
-            "collection": "module-views",
-            "document_id": "60b3e29dc3df330a601b0ef0",
-            "key": "html",
-            "data": {
-                "name": "builder left panel",
-                "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
-                ]
-            }
-        },
-        {
-            "entry": "./demo/components/right-panel.html",
-            "collection": "module-views",
-            "document_id": "5fb16c742fc8e61e01b43c15",
-            "key": "html",
-            "data": {
-                "name": "builder right panel",
-                "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
-                ]
-            }
-        },
-        {
-            "entry": "./demo/components/code-editor.html",
-            "collection": "files",
-            "document_id": "60c01ce2df12fd0d3939ac2b",
-            "key": "src",
-            "data": {
-                "name": "Code Editor",
-                "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
-                ],
-                "path": "/code-editor"
-            }
-        },
-        {
-            "entry": "./demo/components/block-form.html",
-            "collection": "files",
-            "document_id": "60c02a57df12fd0d3939aca0",
-            "key": "src",
-            "data": {
-                "name": "Block Form",
-                "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
-                ],
-                "path": "/block-form"
-            }
-        },
-        {
-            "entry": "./demo/components/app-list.html",
-            "collection": "files",
-            "document_id": "60c624d829e7550bebd6faa7",
-            "key": "src",
-            "data": {
-                "name": "app list",
-                "domains": [
-                    "cocreate.app",
-                    "server.cocreate.app",
-                    "ws.cocreate.app"
-                ],
-                "path": "/applist"
+                "directory": "/docs/builder",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "61381ed8829b690010a4f2b2"
             }
         }
-    ]
+    ],
+
+	"extract": {
+		"directory": "./src/",
+		"extensions": [
+			"js",
+			"css",
+			"html"
+		],
+		"ignores": [
+			"node_modules",
+			"vendor",
+			"bower_components",
+			"archive"
+		]
+	}
 }
+
