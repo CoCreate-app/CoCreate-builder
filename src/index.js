5704d3e9be1789f6e70e08f33d769dfde9c4604e
Frank Pagan <frank@cocreate.app>
2021-10-01 18:44:57 +0000

    fix: cloneELement

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import { canvas, canvasDocument } from './components/resolveCanvas';
 import './components/initAttributes';
 import './components/initDnd';
 import './components/initSelected';
-import './components/initSelect';
 import './components/initToolbar';
+import './components/initSelect';
 import './components/initElementConfig';
-import './components/initCloneElement';
-import './components/initRichText';
+
 
 (async() => {
   // lazy load here
