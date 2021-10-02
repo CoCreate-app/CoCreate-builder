15d79a5779a2bc33509feb18e99f622350a8d15e
Frank Pagan <frank@cocreate.app>
2021-10-02 17:49:30 +0000

    fix: relocated initAttributes.. it is now handled by @cocreate/attributes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { canvas, canvasDocument } from './components/resolveCanvas';
-import './components/initAttributes';
 import './components/initDnd';
 import './components/initSelected';
 import './components/initToolbar';
 import './components/initSelect';
-import './components/initElementConfig';
-
 
 (async() => {
   // lazy load here
