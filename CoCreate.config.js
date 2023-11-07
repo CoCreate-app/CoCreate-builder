581a29cf407a3c9109c5835fab680cc8552055d9
frankpagan <frank@cocreate.app>
2023-11-07 09:47:45 -0600

    fix: update host

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -11,8 +11,7 @@ module.exports = {
                 "path": "/docs/builder/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
-                    "*",
-                    "general.cocreate.app"
+                    "*"
                 ],
                 "directory": "builder",
                 "content-type": "{{content-type}}",
